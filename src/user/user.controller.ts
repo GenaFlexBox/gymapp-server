28778f41df6158a33ba3badc40d3c7b688857352
gn.kopylov <gn.kopylov@qsoft.ru>
2023-11-26 15:54:12 +0500

    add

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -4,4 +4,9 @@ import { UserService } from './user.service';
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
+
+  @Get('status')
+  async status() {
+      return {msg: 'OK'}
+  }
 }
